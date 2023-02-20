8e0eaf84a98d8aac7131365486991a1ee3b3e9bf
Aleksey2710 <Aleksey_2710@mail.ru>
2023-02-21 01:00:22 +0300

    initial commit

---
+++ src/main/java/pro/sky/storeofsocksadd/StoreOfSocksAddApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.storeofsocksadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOfSocksAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreOfSocksAddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/storeofsocksadd/StoreOfSocksAddApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.storeofsocksadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreOfSocksAddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
